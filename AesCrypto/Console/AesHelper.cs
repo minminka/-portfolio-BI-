e66363b421f260fd3c2b218ced441ffded03998c
minminka <119792488+minminka@users.noreply.github.com>
2020-11-15 06:26:02 +0000

    Pontoon: Update Sorbian, Upper (hsb) localization of AMO Co-authored-by: Michael Wolf <milupo@sorbzilla.de>

--- AesCrypto/Console/AesHelper.cs
+++ AesCrypto/Console/AesHelper.cs
@@ -8,4 +8,4 @@ namespace Console
     public static class AesHelper
     {
         private const string IV = "2wDwCbJtSVuTlXhL";
-        private const string KEY = "OZMd2MfM6YuoFNL
+        private const string KEY = "OZMd2MfM6YuoFNLXM50FpJdjX0R9
