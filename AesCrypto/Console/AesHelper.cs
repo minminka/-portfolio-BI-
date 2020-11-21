688ee148063c0b9e0ee0efd0bfb2a11ea9268a9b
minminka <119792488+minminka@users.noreply.github.com>
2020-11-21 07:42:49 +0000

    one more try

--- AesCrypto/Console/AesHelper.cs
+++ AesCrypto/Console/AesHelper.cs
@@ -10,4 +10,4 @@ namespace Console
         private const string IV = "2wDwCbJtSVuTlXhL";
         private const string KEY = "OZMd2MfM6YuoFNLXM50FpJdjX0R926GF";
 
-        public static string Encrypt(thi
+        public static string Encrypt(this string data
