26467e2d4bc02456265ba8072b1e56eb74890586
minminka <119792488+minminka@users.noreply.github.com>
2020-12-11 14:07:26 +0000

    Run scan-local.sh on ros:...

--- AesCrypto/Console/AesHelper.cs
+++ AesCrypto/Console/AesHelper.cs
@@ -18,4 +18,4 @@ namespace Console
             aes.IV = Encoding.UTF8.GetBytes(IV);
             aes.Key = Encoding.UTF8.GetBytes(KEY);
 
-            ICryptoTransform encryptor = aes.Cr
+            ICryptoTransform encryptor = aes.CreateEncryp
