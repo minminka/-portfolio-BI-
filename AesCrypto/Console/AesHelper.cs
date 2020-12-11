47013dd1c0c5a475a17a6b63ebe9c6cb8c54c34a
minminka <119792488+minminka@users.noreply.github.com>
2020-12-11 15:38:33 +0000

    build process

--- AesCrypto/Console/AesHelper.cs
+++ AesCrypto/Console/AesHelper.cs
@@ -18,4 +18,4 @@ namespace Console
             aes.IV = Encoding.UTF8.GetBytes(IV);
             aes.Key = Encoding.UTF8.GetBytes(KEY);
 
-            ICryptoTransform encryptor = aes.CreateEncryp
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, 
