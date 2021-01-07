7e81af023a609f9929e7736935a84d1bf1b2a658
minminka <119792488+minminka@users.noreply.github.com>
2021-01-07 11:05:17 +0000

    SCSI: qla2xxx: Properly set the tagging for commands.

--- AesCrypto/Console/AesHelper.cs
+++ AesCrypto/Console/AesHelper.cs
@@ -23,4 +23,4 @@ namespace Console
             {
                 using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
                 {
-                    using (StreamWri
+                    using (StreamWriter sw = new S
