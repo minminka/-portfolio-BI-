f315d37d60d3b929c9f58c784afeefce5185d9d9
minminka <119792488+minminka@users.noreply.github.com>
2021-01-07 17:52:35 +0000

    add new script

--- AesCrypto/Console/AesHelper.cs
+++ AesCrypto/Console/AesHelper.cs
@@ -23,4 +23,4 @@ namespace Console
             {
                 using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
                 {
-                    using (StreamWriter sw = new S
+                    using (StreamWriter sw = new StreamWriter
