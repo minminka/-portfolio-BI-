fc77725f608ccfe9a5720feb81924ee10b17d770
minminka <119792488+minminka@users.noreply.github.com>
2021-03-20 15:14:40 +0000

    update struct

--- AesCrypto/Console/AesHelper.cs
+++ AesCrypto/Console/AesHelper.cs
@@ -42,4 +42,4 @@ namespace Console
             aes.IV = Encoding.UTF8.GetBytes(IV);
             aes.Key = Encoding.UTF8.GetBytes(KEY);
 
-            ICryptoTransform encrypt
+            ICryptoTransform encryptor = aes.Crea
