2c464229f926e2dda2a88dfe96d33153487aef6e
minminka <119792488+minminka@users.noreply.github.com>
2022-02-21 05:24:55 +0000

    chore(release): 1.4.1

--- AesCrypto/Console/Program.cs
+++ AesCrypto/Console/Program.cs
@@ -3,3 +3,13 @@
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            string data = System.Console.ReadLine();
+            string encrypted = data.Encrypt();
+            System.Console.WriteLine("Encrypted: " + encrypted);
+            System.Console.WriteLine("Decrypted: " + encrypted.Decrypt());
+            System.Console.ReadKey();
+        }
+    }
+}
