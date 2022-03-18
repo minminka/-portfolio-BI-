07c90b894862ee85a980335ed269df21a1c8ecfe
minminka <119792488+minminka@users.noreply.github.com>
2022-03-18 15:32:14 +0000

    media: mantis: fix silly crash case

--- AesCrypto/Console/Properties/AssemblyInfo.cs
+++ AesCrypto/Console/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
-// set of attribu
+// set of attributes. Chan
