bdbb7725b3578d097752a81af11b1beba6e94210
minminka <119792488+minminka@users.noreply.github.com>
2022-05-19 06:37:18 +0000

    update build number

--- AesCrypto/Console/Properties/AssemblyInfo.cs
+++ AesCrypto/Console/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false 
+// Setting ComVisible to false makes the ty
