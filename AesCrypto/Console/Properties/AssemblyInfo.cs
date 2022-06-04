5ac58090c27efd06adc888848b907d795676e7cc
minminka <119792488+minminka@users.noreply.github.com>
2022-06-04 21:18:13 +0000

    Version bump to 0.0.14

--- AesCrypto/Console/Properties/AssemblyInfo.cs
+++ AesCrypto/Console/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a
+// to COM components.  If you need to access a type in this as
