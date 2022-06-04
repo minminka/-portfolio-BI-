f63bc8fb9ac52ef41ee7f99c206c7215484fab6e
minminka <119792488+minminka@users.noreply.github.com>
2022-06-04 06:54:48 +0000

    Update pot

--- AesCrypto/Console/Properties/AssemblyInfo.cs
+++ AesCrypto/Console/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM comp
+// to COM components
