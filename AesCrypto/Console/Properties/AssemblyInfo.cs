79be5805cc8d475abf225306200cd52d8dbbfc85
minminka <119792488+minminka@users.noreply.github.com>
2022-06-15 17:43:28 +0000

    f2fs: introduce NODE_MAPPING for code consistency

--- AesCrypto/Console/Properties/AssemblyInfo.cs
+++ AesCrypto/Console/Properties/AssemblyInfo.cs
@@ -16,4 +16,5 @@ using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on 
+// COM, set the ComVisible attribute to true on that type.
+[a
