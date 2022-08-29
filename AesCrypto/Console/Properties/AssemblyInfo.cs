61d00db0bb704e9f4781a59201c095e52a681a12
minminka <119792488+minminka@users.noreply.github.com>
2022-08-29 20:01:56 +0000

    style fixed

--- AesCrypto/Console/Properties/AssemblyInfo.cs
+++ AesCrypto/Console/Properties/AssemblyInfo.cs
@@ -30,4 +30,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown belo
+// by using the '*' as shown below:
+// [ass
